91f05154135bf265642755afd46ad542c26db66f
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-10 21:30:17 +0200

    Make foundations for lib code

--- client/library/lib.cpp
+++ client/library/lib.cpp
@@ -1,6 +1,37 @@
 #include "lib.hpp"
 #include "src/helper.cpp"
 
-extern int test_func(int x){
-	return my_func(x);
+extern int error;
+
+void NFSClient::connect(char *host, int port, char *user, char *password)
+{
+}
+
+int NFSClient::open(char *path, int oflag, int mode)
+{
+}
+ssize_t NFSClient::read(int fd, void *buf, size_t count)
+{
+}
+ssize_t NFSClient::write(int fd, const void *buf, size_t count)
+{
+}
+off_t NFSClient::lseek(int fd, off_t offset, int whence)
+{
+}
+int NFSClient::close(int fd)
+{
+}
+int NFSClient::unlink(const char *pathname)
+{
+}
+
+int NFSClient::opendir(const char *name)
+{
+}
+dirent *NFSClient::readdir(int dirfd)
+{
+}
+int NFSClient::closedir(int dirfd)
+{
 }
--- client/library/lib.hpp
+++ client/library/lib.hpp
@@ -1,6 +1,25 @@
 #ifndef LIB_HPP
 #define LIB_HPP
 
-extern int test_func(int);
+#include <sys/types.h>
+#include <dirent.h>
+
+extern int error;
+
+class NFSClient {
+    public:
+        void connect(char *host, int port, char *user, char *password);
+
+        int open(char *path, int oflag, int mode);
+        ssize_t read(int fd, void *buf, size_t count);
+        ssize_t write(int fd, const void *buf, size_t count);
+        off_t lseek(int fd, off_t offset, int whence);
+        int close(int fd);
+        int unlink(const char *pathname);
+
+        int opendir(const char *name);
+        dirent *readdir(int dirfd);
+        int closedir(int dirfd);
+};
 
 #endif
