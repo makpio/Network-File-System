df04829869fa904a8557a4f11407775f6cbfa739
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-10 21:53:33 +0200

    Change the code formatting

--- client/library/lib.cpp
+++ client/library/lib.cpp
@@ -1,37 +1,17 @@
 #include "lib.hpp"
-#include "src/helper.cpp"
+#include "src/messeges.cpp"
 
 extern int error;
 
-void NFSClient::connect(char *host, int port, char *user, char *password)
-{
-}
+void NFSClient::connect(char *host, int port, char *user, char *password) {}
 
-int NFSClient::open(char *path, int oflag, int mode)
-{
-}
-ssize_t NFSClient::read(int fd, void *buf, size_t count)
-{
-}
-ssize_t NFSClient::write(int fd, const void *buf, size_t count)
-{
-}
-off_t NFSClient::lseek(int fd, off_t offset, int whence)
-{
-}
-int NFSClient::close(int fd)
-{
-}
-int NFSClient::unlink(const char *pathname)
-{
-}
+int NFSClient::open(char *path, int oflag, int mode) {}
+ssize_t NFSClient::read(int fd, void *buf, size_t count) {}
+ssize_t NFSClient::write(int fd, const void *buf, size_t count) {}
+off_t NFSClient::lseek(int fd, off_t offset, int whence) {}
+int NFSClient::close(int fd) {}
+int NFSClient::unlink(const char *pathname) {}
 
-int NFSClient::opendir(const char *name)
-{
-}
-dirent *NFSClient::readdir(int dirfd)
-{
-}
-int NFSClient::closedir(int dirfd)
-{
-}
+int NFSClient::opendir(const char *name) {}
+dirent *NFSClient::readdir(int dirfd) {}
+int NFSClient::closedir(int dirfd) {}
--- client/library/lib.hpp
+++ client/library/lib.hpp
@@ -1,25 +1,25 @@
 #ifndef LIB_HPP
 #define LIB_HPP
 
-#include <sys/types.h>
 #include <dirent.h>
+#include <sys/types.h>
 
 extern int error;
 
 class NFSClient {
-    public:
-        void connect(char *host, int port, char *user, char *password);
+public:
+  void connect(char *host, int port, char *user, char *password);
 
-        int open(char *path, int oflag, int mode);
-        ssize_t read(int fd, void *buf, size_t count);
-        ssize_t write(int fd, const void *buf, size_t count);
-        off_t lseek(int fd, off_t offset, int whence);
-        int close(int fd);
-        int unlink(const char *pathname);
+  int open(char *path, int oflag, int mode);
+  ssize_t read(int fd, void *buf, size_t count);
+  ssize_t write(int fd, const void *buf, size_t count);
+  off_t lseek(int fd, off_t offset, int whence);
+  int close(int fd);
+  int unlink(const char *pathname);
 
-        int opendir(const char *name);
-        dirent *readdir(int dirfd);
-        int closedir(int dirfd);
+  int opendir(const char *name);
+  dirent *readdir(int dirfd);
+  int closedir(int dirfd);
 };
 
 #endif
