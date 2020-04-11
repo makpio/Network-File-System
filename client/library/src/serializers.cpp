bc218b37093dac609c81fbdb99f621999a21aed0
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-11 15:32:45 +0200

    Make foundation for sercializers.cpp

---
+++ client/library/src/serializers.cpp
@@ -0,0 +1,17 @@
+#ifndef SERIALIZERS_CPP
+#define SERIALIZERS_CPP
+
+#include <vector>
+
+#include "./messages.cpp"
+
+
+std::vector<char> SerializeOpenRequest(OpenRequest open_request) {
+
+}
+
+OpenResponse DeserializeToOpenResponse(std::vector<char> byte_response) {
+
+}
+
+#endif
