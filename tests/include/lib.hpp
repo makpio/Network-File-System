535eeac02f63adc2f8189a5d1ad50d70be31f078
M-Pestka <michal.pestka1@gmail.com>
2020-04-21 21:16:03 +0200

    add include dir

---
+++ tests/include/lib.hpp
@@ -0,0 +1,31 @@
+#ifndef LIB_HPP
+#define LIB_HPP
+
+#include <dirent.h>
+#include <sys/types.h>
+#include <vector>
+
+extern int error_;
+
+class NFSClient {
+public:
+  NFSClient(char *host, int port, char *user, char *password);
+  int connect();
+
+  int open(char *path, int oflag, int mode);
+  ssize_t read(int fd, void *buf, size_t count);
+  ssize_t write(int fd, const void *buf, size_t count);
+  off_t lseek(int fd, off_t offset, int whence);
+  int close(int fd);
+  int unlink(const char *pathname);
+
+  int opendir(const char *name);
+  dirent *readdir(int dirfd);
+  int closedir(int dirfd);
+
+private:
+  void sendRequest_(std::vector<char> request);
+  std::vector<char> receiveResponse_(){return std::vector<char>{};};
+};
+
+#endif
