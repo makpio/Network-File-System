a569dee5373cbae9c760a72c44af52a90f7bf3e4
Michał Kowieski <kowieski.m98@gmail.com>
2020-05-05 00:16:58 +0200

    Update the utils.hpp file

--- tests/utils.hpp
+++ tests/utils.hpp
@@ -15,4 +15,14 @@ std::string bytes_to_string(std::vector<u_int8_t> bytes) {
   return result;
 }
 
+std::string bytes_to_string(char *bytes, int len) {
+  std::stringstream sstream;
+  for (int i = 0; i < len; ++i)
+    sstream << int(*(bytes+i)) << ' ';
+
+  std::string result;
+  getline(sstream, result);
+  return result;
+}
+
 #endif
