42136a8910980ddd4f500f452cd0d9e4ccdad3ed
Michał Kowieski <kowieski.m98@gmail.com>
2020-05-05 13:33:15 +0200

    Refactor the worker.cpp code

--- server/worker.cpp
+++ server/worker.cpp
@@ -9,29 +9,33 @@
 #include "../libraries/core/messages.hpp"
 #include "../libraries/core/serializers.hpp"
 
+std::vector<u_int8_t> open_handler(std::vector<u_int8_t> byte_request) {
+  std::cout << "OPEN" << std::endl;
+
+  OpenRequest request = DeserializeToOpenRequest(byte_request);
+  std::cout << "Request:" << std::endl;
+  std::cout << "  path: " << request.path << std::endl;
+  std::cout << "  oflag: " << request.oflag << std::endl;
+  std::cout << "  mode: " << request.mode << std::endl;
+  int result = open(request.path.c_str(), request.oflag, request.mode);
+
+  OpenResponse response = {result, errno};
+  std::cout << "Response:" << std::endl;
+  std::cout << "  fd: " << response.result << std::endl;
+  std::cout << "  error: " << response.error << std::endl;
+
+  std::vector<u_int8_t> byte_response = SerializeOpenResponse(response);
+  return byte_response;
+}
+
 std::vector<u_int8_t> make_response(std::vector<u_int8_t> byte_request) {
   MessageParser parser = MessageParser(byte_request);
   switch (parser.readMessageType()) {
-
   case MessageType::OPEN_REQUEST:
-    std::cout << "OPEN" << std::endl;
-
-    OpenRequest request = DeserializeToOpenRequest(byte_request);
-    std::cout << "Request:" << std::endl;
-    std::cout << "  path: " << request.path << std::endl;
-    std::cout << "  oflag: " << request.oflag << std::endl;
-    std::cout << "  mode: " << request.mode << std::endl;
-    int result = open(request.path.c_str(), request.oflag, request.mode);
-
-    OpenResponse response = {result, errno};
-    std::cout << "Response:" << std::endl;
-    std::cout << "  fd: " << response.result << std::endl;
-    std::cout << "  error: " << response.error << std::endl;
-
-    std::vector<u_int8_t> byte_response = SerializeOpenResponse(response);
-    return byte_response;
+    return open_handler(byte_request);
+  default:
+    return std::vector<u_int8_t>{0, 0, 0, 0, 0};
   }
-  return std::vector<u_int8_t>{0, 0, 0, 0, 0};
 }
 
 void worker(int socket_fd) {
