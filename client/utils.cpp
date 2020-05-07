b0175aad4bb358402cda8447e33b9987b1524461
M-Pestka <michal.pestka1@gmail.com>
2020-05-07 18:50:08 +0200

    tmp

---
+++ client/utils.cpp
@@ -0,0 +1,17 @@
+#include "utils.h"
+#include <iostream>
+
+
+void display_welcome_message(){
+    std::cout<<"         _   _  ______  _____ "<<'\n';
+    std::cout<<"        | \\ | ||  ____|/ ____|"<<'\n';
+    std::cout<<"  _   _ |  \\| || |__  | (___  "<<'\n';
+    std::cout<<" | | | || . ` ||  __|  \\___ \\ "<<'\n';
+    std::cout<<" | |_| || |\\  || |     ____) |"<<'\n';
+    std::cout<<" | ._,_||_| \\_||_|    |_____/ "<<'\n';
+    std::cout<<" | |                          "<<'\n';
+    std::cout<<" |_|   \n\n\n\n\n"<<std::endl;
+
+
+    std::cout<<"Type 'help' for help :) "<<std::endl;
+}
