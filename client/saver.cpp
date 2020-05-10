672c850e8b091b60c9947a93a047336d9080eddf
M-Pestka <michal.pestka1@gmail.com>
2020-05-10 13:39:58 +0200

    add prining file

--- client/saver.cpp
+++ client/saver.cpp
@@ -1,7 +1,9 @@
 #include "saver.h"
-
+#include "utils.h"
 
 
 bool Mock_saver::save(std::string name, char* buffer, uint len){
+    // TODO: zrobić zapisywanie po kawałku bufora.
     std::cout<<"\nZapisywanie pliku '"+name+"' o wielkosci " + std::to_string(len)<<'\n';
-}
+    std::cout<<convertToString(buffer, len)<<'\n'; 
+}  
