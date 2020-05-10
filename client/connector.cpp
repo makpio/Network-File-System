6f7fc6adb84d407560b2ae0a65e0cd1228d02b7f
M-Pestka <michal.pestka1@gmail.com>
2020-05-10 13:40:45 +0200

    add basic function

--- client/connector.cpp
+++ client/connector.cpp
@@ -5,14 +5,33 @@ int LocalFSConnector::connect(std::string host, uint port, std::string user, std
 }
 
 FileDescriptor LocalFSConnector::open(std::string path, int oflag, int mode){
-    std::fstream *s = new std::fstream(path);
-    FileDescriptor fd = open_files.rbegin()->first + 1;
+    std::fstream *s = new std::fstream(path, std::ios::in);
+    FileDescriptor fd;
+    if(open_files.empty()){
+        fd = 1;
+    }else{
+        fd = open_files.rbegin()->first + 1;
+    }
+    
     open_files[fd] = s;
+    return fd;
+}
+
+
+ssize_t LocalFSConnector::read(FileDescriptor fd, char *buf, size_t count){
+    std::fstream* s = open_files[fd];
+    s->get(buf, count);
+    return s->gcount();
 }
 
 int LocalFSConnector::close(FileDescriptor fd){
     open_files[fd]->close();
     delete open_files[fd];
     open_files.erase(fd);
+}
 
+LocalFSConnector::~LocalFSConnector(){
+    for (auto iter = open_files.rbegin(); iter != open_files.rend(); ++iter) {
+        close(iter->first);
+    }
 }
--- client/connector.h
+++ client/connector.h
@@ -10,23 +10,26 @@ using FileDescriptor = int;
 
 class Connector{
 public:
+    
     virtual int connect(std::string host, uint port, std::string user, std::string passwd)=0;
     virtual int open(std::string path, int oflag, int mode)=0;
-    virtual ssize_t read(FileDescriptor fd, void *buf, size_t count)=0;
-    virtual ssize_t write(FileDescriptor fd, const void* buf, size_t count)=0;
+    virtual ssize_t read(FileDescriptor fd, char *buf, size_t count)=0;
+    virtual ssize_t write(FileDescriptor fd, const char* buf, size_t count)=0;
     virtual int close(FileDescriptor fd)=0;
 };
+
 
 class LocalFSConnector: public Connector{
 public:
     int connect(std::string host, uint port, std::string user, std::string passwd);
     FileDescriptor open(std::string path, int oflag, int mode);
-    ssize_t read(FileDescriptor fd, void *buf, size_t count){};
-    ssize_t write(FileDescriptor fd, const void* buf, size_t count){};
+    ssize_t read(FileDescriptor fd, char *buf, size_t count);
+    ssize_t write(FileDescriptor fd, const char* buf, size_t count){return 0;};
     int close(FileDescriptor fd);
 
+    ~LocalFSConnector();
 private:
-    std::map<FileDescriptor, std::fstream*> open_files; 
+    std::map<FileDescriptor, std::fstream*> open_files{}; 
 };
 
 
