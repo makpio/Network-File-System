abe71014af4067ac4924a25c13e8d2aa8bd86149
M-Pestka <michal.pestka1@gmail.com>
2020-05-13 19:11:53 +0200

    add put_handler declaration

--- client/command_handlers/handlers.h
+++ client/command_handlers/handlers.h
@@ -2,9 +2,12 @@
 #define GET_HANDLER_H
 #include "../command.h"
 #include "../saver.h"
+#include "../reader.h"
 #include "../connector.h"
 
 bool get_handler(Command c, Saver *saver, Connector *);
+
+bool put_handler(Command c, Reader *reader, Connector *);
 
 
 #endif
