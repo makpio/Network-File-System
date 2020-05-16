353aa1b81e145bf66812cc2bd5d34e526a0ae9f0
Mateusz <matiziel10@gmail.com>
2020-05-17 01:39:27 +0200

    Add handler class with descriptor mapper

--- server/handler.cpp
+++ server/handler.cpp
@@ -7,7 +7,7 @@
 #include "../libraries/core/serializers.hpp"
 
 
-std::vector<u_int8_t> open_handler(std::vector<u_int8_t> byte_request) {
+std::vector<u_int8_t> Handler::open_handler(std::vector<u_int8_t> byte_request) {
     std::cout << "OPEN" << std::endl;
 
     OpenRequest request = DeserializeToOpenRequest(byte_request);
@@ -17,7 +17,9 @@ std::vector<u_int8_t> open_handler(std::vector<u_int8_t> byte_request) {
     std::cout << "  mode: " << request.mode << std::endl;
     int result = open(request.path.c_str(), request.oflag, request.mode);
 
-    OpenResponse response = {result, errno};
+    int fileFd = mapper.addDescriptor(result);
+
+    OpenResponse response = {fileFd, errno};
     std::cout << "Response:" << std::endl;
     std::cout << "  fd: " << response.result << std::endl;
     std::cout << "  error: " << response.error << std::endl;
@@ -27,7 +29,7 @@ std::vector<u_int8_t> open_handler(std::vector<u_int8_t> byte_request) {
     return byte_response;
 }
 
-std::vector<u_int8_t> read_handler(std::vector<u_int8_t> byte_request) {
+std::vector<u_int8_t> Handler::read_handler(std::vector<u_int8_t> byte_request) {
     std::cout << "READ" << std::endl;
 
     ReadRequest request = DeserializeToReadRequest(byte_request);
@@ -49,13 +51,19 @@ std::vector<u_int8_t> read_handler(std::vector<u_int8_t> byte_request) {
     return byte_response;
 }
 
-std::vector<u_int8_t> make_response(std::vector<u_int8_t> byte_request) {
+std::vector<u_int8_t> Handler::authenticate_handler(std::vector<u_int8_t> byte_request){
+
+}
+
+std::vector<u_int8_t> Handler::make_response(std::vector<u_int8_t> byte_request) {
     MessageParser parser = MessageParser(byte_request);
     switch (parser.readMessageType()) {
         case MessageType::OPEN_REQUEST:
             return open_handler(byte_request);
         case MessageType::READ_REQUEST:
             return read_handler(byte_request);
+        case MessageType::AUTHENTICATE_REQUEST:
+            return authenticate_handler(byte_request);
         default:
             return std::vector<u_int8_t>{0, 0, 0, 0, 0};
     }
--- server/handler.h
+++ server/handler.h
@@ -1,10 +1,19 @@
 #ifndef NFS_HANDLER_H
 #define NFS_HANDLER_H
 
-std::vector<u_int8_t> open_handler(std::vector<u_int8_t> byte_request);
+#include "DescriptorsMapper.h"
 
-std::vector<u_int8_t> read_handler(std::vector<u_int8_t> byte_request);
+class Handler {
+public:
+    std::vector<u_int8_t> open_handler(std::vector<u_int8_t> byte_request);
+    std::vector<u_int8_t> read_handler(std::vector<u_int8_t> byte_request);
+    std::vector<u_int8_t> authenticate_handler(std::vector<u_int8_t> byte_request);
+    std::vector<u_int8_t> make_response(std::vector<u_int8_t> byte_request);
 
-std::vector<u_int8_t> make_response(std::vector<u_int8_t> byte_request);
+private:
+    DescriptorsMapper mapper;
+};
+
+
 
 #endif //NFS_HANDLER_H
