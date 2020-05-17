e53de2882e7b792aecb3ef1bdf03b2276772aae9
M-Pestka <michal.pestka1@gmail.com>
2020-05-17 21:04:15 +0200

    improve commandline characteristics - autocompletion and persistent command history

--- client/linenoise_setup.h
+++ client/linenoise_setup.h
@@ -31,11 +31,20 @@ namespace linenoise_setup{
 
         return NULL;
     }
+    void completion(const char *buf, linenoiseCompletions *lc) {
+    if (buf[0] == 'r') {
+        linenoiseAddCompletion(lc,"record");
+    }
+}
+
+
 };
 
 void custom_linenoise_setup(unsigned int hist_size = 10){
     linenoiseSetHintsCallback(linenoise_setup::hints);
+    linenoiseSetCompletionCallback(linenoise_setup::completion);
     linenoiseHistorySetMaxLen(10);
+    int success = linenoiseHistoryLoad(".uNSFcommandhistory.txt");
 }
 
 
